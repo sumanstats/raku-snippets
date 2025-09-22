51490a509509e8591c280a5e55fa380baba4ee7f
Suman Khanal <suman81765@gmail.com>
2025-09-22 18:16:25 +0545

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -179,6 +179,8 @@ Interactive uses `nil` for the require-match argument so new files can be create
 (prefer-coding-system 'utf-8)
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
+;; Use pandoc for markdown conversion
+(setq markdown-command "pandoc -f gfm -t html5 --standalone")
 
 
 
