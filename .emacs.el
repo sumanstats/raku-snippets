ee13cbcc189fa801b3862e0f93d3a0224b2f411e
Suman Khanal <suman81765@gmail.com>
2025-10-10 19:11:42 +0545

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -341,8 +341,8 @@ Interactive uses `nil` for the require-match argument so new files can be create
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(anzu company-statistics ess markdown-mode monokai-theme
-	  move-text neotree smartparens solarized-theme yasnippet)))
+   '(anzu ess markdown-mode monokai-theme
+	  move-text color-theme-sanityinc-tomorrow quarto-mode smartparens solarized-theme yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
